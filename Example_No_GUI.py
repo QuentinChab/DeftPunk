a4db1bb96e4d867c86643c28991081658d07e3d0
Quentin <quentin.chaboche@gmail.com>
2025-08-04 12:24:17 +0200

    Use coupled parameters in example

--- Example_No_GUI.py
+++ Example_No_GUI.py
@@ -13,43 +13,41 @@ import sys
 function_directory = os.getcwd()
 sys.path.append(function_directory+os.sep+'functions'+os.sep)
 
-from compute_anisotropy import get_anisotropy, trackmap
+from compute_anisotropy import analyze_image, get_anisotropy, trackmap
 
 ############################## USER PART ######################################
 
 # pathes
-imgpath = './MT_kinesin_blue.tif'
-savedir = '../defect_analysis_documents/test_outputs'
-framesave = savedir+os.sep+'frames'
+imgpath     = os.getcwd()+os.sep+'MT_kinesin_blue.tif'
+savedir     = '/home/quentin/Documents'# Path to where you want to save data/image outputs
+framesave   = savedir+os.sep+'frames' 
+stack       = True
 
-# display option
-display_track = True
-
-# director field computation parameters
-stack   = True  # is the image a stack?
-sigma   = 8    # Size of window for director field computation
-bin_    = 4     # Binning of the field wrt to image px
-
-# Defect detection parameters
-fov     = 2   # Size of the averaging gaussian window used on the field for nematic order parameter estimation
-BoxSize = 8     # Size of box contour for charge computation
-order_threshold = 0.5 # Threshold nematic order parameter under which we estimate a defect is present
-peak_threshold  = 0.85 # Angle threshold for charge computation
-
-# Anisotropy computation parameters
-R = 40    # Size of intergation contour around defect to compute anisotropy
+# Detection parameters
+feature_size    = 8
+order_threshold = 0.5
+R               = 15
 
 # Tracking parameters
 sR     = 30
 memory = 5
 filt   = 5
 
+# display option
+display_track = True
+
+
+
+
 ########################### functions call ####################################
 
 # Performing detection
 if not os.path.exists(savedir):
     os.makedirs(savedir)
-e_vec, err_vec, cost_vec, theta_vec, phi, defect_char = get_anisotropy(imgpath, False, R, sigma, bin_, fov, BoxSize, order_threshold, peak_threshold, stack=stack, savedir = savedir)
+e_vec, err_vec, cost_vec, theta_vec, phi, defect_char = analyze_image(imgpath, feature_size, R, order_threshold, stack=stack, savedir = savedir)
+
+
+#e_vec, err_vec, cost_vec, theta_vec, phi, defect_char = get_anisotropy(imgpath, False, R, feature_size, bin_, fov, BoxSize, order_threshold, peak_threshold, stack=stack, savedir = savedir)
 
 # Tracking
 defect_char = tp.link(defect_char, sR, memory=memory)
